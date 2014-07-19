1e7ca415720c4651b6a15f72cebe7c259a31b886
Callum Dickinson <callum@huttradio.co.nz>
2014-07-19 13:24:10 +1200

    Install kadmin-server

---
+++ manifests/kadmin_server.pp
@@ -0,0 +1,51 @@
+# == Class: kerberos
+#
+# Full description of class kerberos here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
+#
+# === Examples
+#
+#  class { kerberos:
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
+#
+# === Authors
+#
+# Author Name <author@domain.com>
+#
+# === Copyright
+#
+# Copyright 2014 Your name here, unless otherwise noted.
+#
+class kerberos::kadmin_server
+(
+	# Parameters retrieved from kerberos::params.
+	$kadmin_server_packages	= $kerberos::params::kadmin_server_packages
+) inherits kerberos::params
+{
+	require kerberos::kdc
+
+	# Install the Kerberos kadmind packages.
+	package
+	{ $kadmin_server_packages:
+		ensure	=> installed,
+	}
+}
